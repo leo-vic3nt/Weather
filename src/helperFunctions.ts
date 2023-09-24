06325736341cc9fec7fd51f1a027866f685f1976
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-09-24 01:12:29 -0300

    Add getDateFormated function

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -9,3 +9,17 @@ export function sleep(delay: number): Promise<void> {
         setTimeout(resolve, delay);
     });
 }
+
+export function getDateFormated(): string {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+
+    const currentDate = new Date();
+    const day = days[currentDate.getDay()];
+    const dayNr = currentDate.getDate();
+    const month = months[currentDate.getMonth()];
+    const year = currentDate.getFullYear();
+
+    return `${day}, ${dayNr} ${month} ${year}`;
+}
+
