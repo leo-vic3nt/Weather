87ecad3b5d35c782b5829c44264caaf9c8aee398
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-10-02 00:35:25 -0300

    Adjust images and implement function to apply them

--- src/components/HeroSection/heroSection.ts
+++ src/components/HeroSection/heroSection.ts
@@ -2,17 +2,15 @@ import { WeatherData } from "../../weatherApiInterfaces";
 import { BACKGROUND_IMGS } from "../../imagesMapping";
 
 export function renderBackgroundImages(weatherData: WeatherData): void {
-    const bodyElement = document.querySelector("body") as HTMLBodyElement;
     const heroSectionElement = document.getElementById("heroSection") as HTMLDivElement;
 
     const isDay = weatherData.current.is_day;
     const currentCondition = weatherData.current.condition.text;
+    const gradientFilter = "linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4))";
 
     if (isDay) {
-        bodyElement.style.backgroundImage = `url(${BACKGROUND_IMGS.day[currentCondition]})`;
-        heroSectionElement.style.backgroundImage = `url(${BACKGROUND_IMGS.day[currentCondition]})`;
+        heroSectionElement.style.backgroundImage = `${gradientFilter},url(${BACKGROUND_IMGS.day[currentCondition]})`;
     } else {
-        bodyElement.style.backgroundImage = `url(${BACKGROUND_IMGS.night[currentCondition]})`;
-        heroSectionElement.style.backgroundImage = `url(${BACKGROUND_IMGS.night[currentCondition]})`;
+        heroSectionElement.style.backgroundImage = `${gradientFilter},url(${BACKGROUND_IMGS.day[currentCondition]})`;
     }
 }
