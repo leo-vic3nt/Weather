24cea1f22f8c434d4b84ea5ea8c4af79ce083b18
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-10-07 00:48:18 -0300

    Change icons to svgs and some design refactors

--- src/components/HeroCard/heroCard.ts
+++ src/components/HeroCard/heroCard.ts
@@ -13,8 +13,13 @@ function renderGreetings(weatherData: WeatherData) {
 }
 
 function renderMainStats(weatherData: WeatherData) {
+    const temperature = document.querySelector(".hero-card__temperature-value") as HTMLParagraphElement;
+
+    temperature.insertAdjacentText("afterbegin", `${weatherData.current.temp_c}`);
+}
+
+function renderSecondaryStats(weatherData: WeatherData) {
     const conditionIcon = document.querySelector(".hero-card__main-icon") as HTMLImageElement;
-    const temperature = document.querySelector(".hero-card__temperature") as HTMLParagraphElement;
     const currentConditionText = weatherData.current.condition.text;
 
     if (weatherData.current.is_day) {
@@ -23,30 +28,18 @@ function renderMainStats(weatherData: WeatherData) {
         conditionIcon.src = WEATHER_ICONS.night[currentConditionText];
     }
 
-    temperature.textContent = `${weatherData.current.temp_c}ºC`;
-}
-
-function renderSecondaryStats(weatherData: WeatherData) {
-    const humidityIcon = document.querySelector(".hero-card__humidity-icon") as HTMLImageElement;
     const humidityText = document.querySelector(".hero-card__humidity-text") as HTMLParagraphElement;
-    humidityIcon.src = "/icons/Stats/humidity.png";
     humidityText.textContent = `${weatherData.current.humidity}%`;
 
-    const chanceOfRainIcon = document.querySelector(".hero-card__chance-of-rain-icon") as HTMLImageElement;
     const chanceOfRainText = document.querySelector(".hero-card__chance-of-rain-text") as HTMLParagraphElement;
-    chanceOfRainIcon.src = "/icons/Stats/chance-of-rain.png";
     // foracastday[0] means today
     chanceOfRainText.textContent = `${weatherData.forecast.forecastday[0].day.daily_chance_of_rain}%`;
 
-    const windSpeedIcon = document.querySelector(".hero-card__wind-speed-icon") as HTMLImageElement;
     const windSpeedText = document.querySelector(".hero-card__wind-speed-text") as HTMLParagraphElement;
-    windSpeedIcon.src = "/icons/Stats/wind-speed.png";
     windSpeedText.textContent = `${weatherData.current.wind_kph.toFixed(0)}  km/h`;
 
-    const uvIndexIcon = document.querySelector(".hero-card__uv-index-icon") as HTMLImageElement;
     const uvIndexText = document.querySelector(".hero-card__uv-index-text") as HTMLParagraphElement;
-    uvIndexIcon.src = "/icons/Stats/uv-index.png";
-    uvIndexText.textContent = `${weatherData.current.uv}`;
+    uvIndexText.textContent = `${weatherData.current.uv}.0`;
 }
 
 export function renderHeroCard(weatherData: WeatherData) {
