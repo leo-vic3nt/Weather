55dfc87e3172b03a50c4e169902c6d0b7bdd38f1
Leonardo Vicente <leonardo.vic3nte@gmail.com>
2023-10-09 18:42:41 +0000

    Add replace to reduce verbosity from api response

--- src/components/HeroCard/heroCard.ts
+++ src/components/HeroCard/heroCard.ts
@@ -8,7 +8,11 @@ function renderGreetings(weatherData: WeatherData) {
     const locationName = weatherData.location.name;
     subtitle.textContent = `Today in ${locationName}`;
 
-    const currentCondition = weatherData.current.condition.text;
+    // Remove some unecessary verbosity from api response
+    const currentCondition = weatherData.current.condition.text
+        .replace(/(Patchy|Possible|at times|Moderate or heavy)/gi, "")
+        .trim();
+
     title.textContent = currentCondition;
 }
 
