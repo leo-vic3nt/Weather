f616093b036f835c40986f45477eef6149b9ce3f
Leonardo Vicente <leonardo.vic3nte@gmail.com>
2023-10-10 18:57:00 +0000

    Implement request flow on main

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,22 @@ import { renderHeroCard } from "./components/HeroCard/heroCard";
 import { innitLocationSearch } from "./components/SearchBar/searchBar";
 import { renderBackgroundImage, removeLoading } from "./helperFunctions";
 import { getWeatherByGeolocation, getWeatherByIp, getWeatherData } from "./weatherApiFunctions";
+import { WeatherData } from "./weatherApiInterfaces";
 
 innitLocationSearch();
 
-getWeatherData("Iguatu")
-    .then((res) => {
-        renderHeroCard(res);
-        renderBackgroundImage(res);
-        displayDateTime(res);
-        removeLoading();
-    })
+function renderPage(data: WeatherData): void {
+    renderHeroCard(data);
+    renderBackgroundImage(data);
+    displayDateTime(data);
+    removeLoading();
+}
 
+getWeatherByGeolocation()
+    .then((res) => renderPage(res))
+    .catch((err) => {
+        console.error(err);
+        getWeatherByIp()
+            .then((res) => renderPage(res))
+            .catch(() => getWeatherData("Fortaleza"));
+    });
