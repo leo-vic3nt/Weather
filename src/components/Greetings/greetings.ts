6abe707d96d7ae6f198dedecf0382eae69da9076
Leonardo Vicente <leonardo.vic3nte@gmail.com>
2023-10-23 18:08:16 +0000

    Start implement the day pickers component

--- src/components/Greetings/greetings.ts
+++ src/components/Greetings/greetings.ts
@@ -3,11 +3,11 @@ import { WeatherData } from "../../ts/weatherApiInterfaces";
 export { renderGreetings };
 
 function renderGreetings(weatherData: WeatherData) {
-    const subtitle = document.querySelector(".greetings__subtitle") as HTMLHeadingElement;
     const title = document.querySelector(".greetings__title") as HTMLHeadingElement;
-
+    const locationElement = document.querySelector(".greetings__location") as HTMLSpanElement;
     const locationName = weatherData.location.name;
-    subtitle.textContent = `Today in ${locationName}`;
+
+    locationElement.textContent = ` in ${locationName}`;
 
     // Remove some unecessary verbosity from api response
     const currentCondition = weatherData.current.condition.text
